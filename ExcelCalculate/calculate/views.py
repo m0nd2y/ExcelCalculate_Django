54bb4e9c522728079562fa371484d0c36053e505
m0nd2y <ldj6192@gmail.com>
2021-08-08 17:00:53 +0900

    'commit'

--- ExcelCalculate/calculate/views.py
+++ ExcelCalculate/calculate/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def calculate(request) :
+    file = request.POST['fileInput']
     return HttpResponse("calculate, calculate function!")
