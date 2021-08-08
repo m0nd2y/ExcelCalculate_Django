f631b3e282accdb25084bbfd837297020681d95c
m0nd2y <ldj6192@gmail.com>
2021-08-08 12:47:43 +0900

    'commit'

--- ExcelCalculate/calculate/views.py
+++ ExcelCalculate/calculate/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def calculate(request) :
+    return HttpResponse("calculate, calculate function!")
