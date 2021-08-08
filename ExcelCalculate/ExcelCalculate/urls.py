99e58afa812ed30bc7541a1d394a1560bb3d9451
m0nd2y <ldj6192@gmail.com>
2021-08-08 12:45:25 +0900

    'commit'

--- ExcelCalculate/ExcelCalculate/urls.py
+++ ExcelCalculate/ExcelCalculate/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('email/', include('sendEmail.urls'), name='email'),
     path('calculate/', include('calculate.urls'), name='calculate'),
-    path('', include('main.urls'), name='main')
+    path('', include('main.urls'), name='main'),
     path('admin/', admin.site.urls),
 ]
---
+++ ExcelCalculate/sendEmail/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+urlpatterns = [
+    path('send', views.send, name='email_send'),
+]
