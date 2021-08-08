5b32a5398b530e8cac2a9633d3d78a7852e9c281
m0nd2y <ldj6192@gmail.com>
2021-08-08 12:38:48 +0900

    'commit'

--- ExcelCalculate/ExcelCalculate/urls.py
+++ ExcelCalculate/ExcelCalculate/urls.py
@@ -17,5 +17,8 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('email/', include('sendEmail.urls'), name='email'),
+    path('calculate/', include('calculate.urls'), name='calculate'),
+    path('', include('main.urls'), name='main')
     path('admin/', admin.site.urls),
 ]
