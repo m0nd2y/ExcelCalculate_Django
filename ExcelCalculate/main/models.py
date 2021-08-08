cea5b9418cfd9d5031cf7bb61fcbcfb0b6db2552
m0nd2y <ldj6192@gmail.com>
2021-08-08 13:48:26 +0900

    'commit'

--- ExcelCalculate/main/models.py
+++ ExcelCalculate/main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class User(models.Model):
     user_name = models.CharField(max_length=30)
     user_email = models.EmailField(unique=True)
