118729b30b044bc3c28e39457b0f264f381b1565
m0nd2y <ldj6192@gmail.com>
2021-08-08 13:02:01 +0900

    'commit'

--- ExcelCalculate/main/views.py
+++ ExcelCalculate/main/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'index.html')
 
 def signup(request):
-    return render(request, 'main/signup.html')
+    return render(request, 'signup.html')
 
 def signin(request):
-    return render(request, 'main/signin.html')
+    return render(request, 'signin.html')
 
 def verifyCode(request):
-    return render(request, 'main/verifyCode.html')
+    return render(request, 'verifyCode.html')
 
 def verify(request):
     return redirect('main_index')
 
 def result(request):
-    return render(request, 'main/result.html')
+    return render(request, 'result.html')
