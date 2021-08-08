366f3881ef57c9565eff97b8b6dae2cd9a9937fb
m0nd2y <ldj6192@gmail.com>
2021-08-08 15:05:49 +0900

    'commit'

--- ExcelCalculate/main/urls.py
+++ ExcelCalculate/main/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('signup', views.signup, name='main_signup'),
     path('signup/join', views.join, name='main_join'),
     path('signin', views.signin, name='main_signin'),
+    path('signin/login', views.signin, name='main_signin'),
     path('verifyCode', views.verifyCode, name='main_verifyCode'),
     path('verify', views.verify, name='main_verify'),
     path('result', views.result, name='main_result'),
