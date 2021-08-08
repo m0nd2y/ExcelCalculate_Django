5e0f29af31d1a7f3c0f5fb4e02a1f242ef7d8c00
m0nd2y <ldj6192@gmail.com>
2021-08-08 12:55:39 +0900

    'commit'

--- ExcelCalculate/sendEmail/views.py
+++ ExcelCalculate/sendEmail/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def send(receiverEmail, verifyCode) :
-    return HttpResponse("sendEmail, send function!")
+    return HttpResponse("sendEmail, send function!")    
